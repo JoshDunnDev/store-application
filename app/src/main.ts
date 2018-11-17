99174d3c88394dd94a120ae0df2eabf0fe2814a0
Josh Dunn <josh@joshdunn.net>
2018-11-17 17:42:13 -0600

    Reformatted project to Angular CLI to make it easier for Docker. Updated HTML & CSS files to continue with page layout.

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,12 @@
-// Import global stylesheet
-import './styles/main.css';
-
-//Need to change for derployment to { platformBrowser } from '@angular/platform-browser x'
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
