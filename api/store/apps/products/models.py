6ed9bacfb1f08e951bd8c873d511a3d781ec4467
TonyDuran <antonio.e.duran@outlook.com>
2018-11-20 18:14:00 -0600

    Adding api endpoint for getting product data. Far from finished.

--- api/store/apps/products/models.py
+++ api/store/apps/products/models.py
@@ -2,7 +2,11 @@
 from django.db import models
 
 
-class Product():
+class Product(models.Model):
 
     name = models.CharField(max_length=40, blank=True)
     description = models.CharField(max_length=200, blank=True)
+    category = models.CharField(max_length=60, blank=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    product_size = models.CharField(max_length=60, blank=False)
+    date_created = models.DateField()
--- api/store/apps/products/views.py
+++ api/store/apps/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse
+from store.apps.products.models import Product
+from django.http import JsonResponse
 
 # Create your views here.
+def get_products(request):
+    products = serializers.serialize('json',Product.objects.all())
+    return HttpResponse(products, content_type="application/json")
