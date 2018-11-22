6128f694e36c51c5994c82c537f3a77e226122c9
TonyDuran <antonio.e.duran@outlook.com>
2018-11-22 13:48:21 -0600

    Adding script to test db connection before attempting manage.py. Should fix/help the django container from crashing when loading before db container. Also, updated example loaddata json file.

---
+++ api/scripts/psqltest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import psycopg2
+import time
+import os
+t_end = time.time() + 30
+while (time.time() < t_end):
+    try:
+        db = psycopg2.connect("dbname='postgres' user='postgres' host='db'")
+        print("OK!")
+        os.system("python manage.py makemigrations")
+        os.system("python manage.py migrate")
+        # os.system("python manage.py loaddata scripts/product_data.json")
+        os.system("python manage.py runserver 0.0.0.0:8000")
+        break;
+    except:
+        # print("Trying again!")
+        continue;
+else:
+    print("FAIL!")
+    exit(1)
